182d1541c4c7c1c8db11294a2e6f93a494b5e78e
Eitan Fire <eitanfire@gmail.com>
2025-07-02 23:53:06 -0600

    add statement

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <App /> hello world
+    
   </React.StrictMode>
 );
